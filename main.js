4cfe1fcf301a7b2e73d8621c3e95fd0d3358ea91
ZachK26 <63168062+ZachK26@users.noreply.github.com>
2021-06-01 14:24:28 -0500

    finished main feature
    added hide button and gave functionality to rest of notecards

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 var counter = 0;
+var counterTwo = 0;
+var counterThree = 0;
+var counterFour = 0;
+var counterFive = 0;
 
 document.getElementById("submitOne").onclick = function noteCards (){
     document.getElementById("cardOneTerm").innerHTML = document.getElementById("termOne").value;
     counter = 0;
     }
+document.getElementById("submitTwo").onclick = function noteCards (){
+        document.getElementById("cardTwoTerm").innerHTML = document.getElementById("termTwo").value;
+        counterTwo = 0;
+        }
+document.getElementById("submitThree").onclick = function noteCards (){
+            document.getElementById("cardThreeTerm").innerHTML = document.getElementById("termThree").value;
+            counterThree = 0;
+            }
+document.getElementById("submitFour").onclick = function noteCards (){
+                document.getElementById("cardFourTerm").innerHTML = document.getElementById("termFour").value;
+                counterFour = 0;
+                }
+document.getElementById("submitFive").onclick = function noteCards (){
+                    document.getElementById("cardFiveTerm").innerHTML = document.getElementById("termFive").value;
+                    counterFive = 0;
+                    }
 
 
 
@@ -16,6 +36,48 @@ else {
     document.getElementById("cardOneTerm").innerHTML = document.getElementById("termOne").value;  
 }
 counter++;
-
-
 }
+document.getElementById("cardTwoTerm").onclick = function flip (){
+    if ((counterTwo%2) == 0){
+        document.getElementById("cardTwoTerm").innerHTML = document.getElementById("definitionTwo").value  
+    }
+    else {
+        document.getElementById("cardTwoTerm").innerHTML = document.getElementById("termTwo").value;  
+    }
+    counterTwo++;
+    }
+document.getElementById("cardThreeTerm").onclick = function flip (){
+        if ((counterThree%2) == 0){
+            document.getElementById("cardThreeTerm").innerHTML = document.getElementById("definitionThree").value  
+        }
+        else {
+            document.getElementById("cardThreeTerm").innerHTML = document.getElementById("termThree").value;  
+        }
+        counterThree++;
+        }
+document.getElementById("cardFourTerm").onclick = function flip (){
+            if ((counterFour%2) == 0){
+                document.getElementById("cardFourTerm").innerHTML = document.getElementById("definitionFour").value  
+            }
+            else {
+                document.getElementById("cardFourTerm").innerHTML = document.getElementById("termFour").value;  
+            }
+            counterFour++;
+            }
+document.getElementById("cardFiveTerm").onclick = function flip (){
+                if ((counterFive%2) == 0){
+                    document.getElementById("cardFiveTerm").innerHTML = document.getElementById("definitionFive").value  
+                }
+                else {
+                    document.getElementById("cardFiveTerm").innerHTML = document.getElementById("termFive").value;  
+                }
+                counterFive++;
+                }
+                document.getElementById("hideMeButton").onclick = function flip (){
+                    var x = document.getElementById("hideMe");
+                    if (x.style.display === "none") {
+                      x.style.display = "block";
+                    } else {
+                      x.style.display = "none";
+                    }
+                  }
